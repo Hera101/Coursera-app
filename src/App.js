c176ab70ea0262f052549ddc0b3cc995ef4589a5
Alexandra <amonje1026@gmail.com>
2023-07-12 12:17:59 -0400

    added semantic tags and OGP

--- src/App.js
+++ src/App.js
@@ -2,14 +2,57 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Homepage!
-        </p>
-      </header>
-    </div>
+    <>
+     <Header>
+      <img src="%PUBLIC_URL%/logo.png"/>
+     </Header>
+      <Nav>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+         </li>
+        <li>
+           <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+           <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+           <a href="/orderonline">Order Online</a>
+        </li>
+        <li>
+           <a href="/login">Login</a>
+        </li>
+    </ul>
+</Nav>
+      <Main></Main>
+      <Footer>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+         </li>
+        <li>
+           <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+           <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+           <a href="/orderonline">Order Online</a>
+        </li>
+        <li>
+           <a href="/login">Login</a>
+        </li>
+    </ul>
+      </Footer>
+      </>
   );
-}
+ }
 
 export default App;
